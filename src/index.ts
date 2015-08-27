219203bfb12e09c3f593f36338a355f439e2ff0c
Dave Willmer <dave.willmer@gmail.com>
2015-08-27 00:09:23 +0000

    Fix interfaces

--- src/index.ts
+++ src/index.ts
@@ -35,40 +35,40 @@ interface ICommand {
    * see fit. This hierarchical naming may be useful for menu generation,
    * or may allow faster searching in a command-pallette-style search.
    */
-  name: string,
+  name: string;
 
   /**
    * The callable/function/closure representing the work to be performed
    * when this command is required.
    */
-  callable: any,
+  callable: any;
 
   /**
    * An (optional) short description of what this command does.
    * This could be used to populate the text on the menu item, for example, if
    * the command is to be used through a UI menu bar.
    */
-  short_desc?: string,
+  short_desc?: string;
 
   /**
    * An (optional) long description of what this command does.
    * This could be used in tooltips / command palettes, or just for
    * general fuzzy search, to give more relevant terms.
    */
-  long_desc?: string,
+  long_desc?: string;
 
   /**
    * An (optional) string denoting the shortcut for this specific command.
    * In the case of a command to be shown in a menu, this could
    * be something like 'Ctrl-Shift-T'
    */
-  shortcut?: string,
+  shortcut?: string;
 
   /**
    * This is a menubar-specific array of strings to denote the location
    * in the menu hierarchy where this command should be placed.
    */
-  menu_location?: string[]
+  menu_location?: string[];
 
   /**
    * Menu constraints are lists of items which denote the position of
@@ -88,7 +88,7 @@ interface ICommand {
    * location, but that is implementation-specific.
    *
    */
-  menu_constraints?: any
+  menu_constraints?: any;
 }
 
 /**
